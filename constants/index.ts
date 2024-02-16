9f82d2b9a25a53c42f3b4faaf6124f6ef50ed716
Miguel Alino <miggyalino35@gmail.com>
2024-02-16 20:24:24 +0800

    Set up

---
+++ constants/index.ts
@@ -0,0 +1,78 @@
+export const PRODUCTS = [
+    {
+      title: "Moonbeam",
+      link: "https://gomoonbeam.com",
+      thumbnail:"/rhyssbold.jpg",
+    },
+    {
+      title: "Cursor",
+      link: "https://cursor.so",
+      thumbnail:"/rhyssbold.jpg",
+    },
+    {
+      title: "Rogue",
+      link: "https://userogue.com",
+      thumbnail:"/rhyssbold.jpg",
+    },
+    {
+        title: "Rogue",
+        link: "https://userogue.com",
+        thumbnail:"/rhyssbold.jpg",
+    },
+    {
+      title: "Editrix AI",
+      link: "https://editrix.ai",
+      thumbnail:'/rhyssbold.jpg',
+    },
+    {
+        title: "Moonbeam",
+        link: "https://gomoonbeam.com",
+        thumbnail:"/rhyssbold.jpg",
+      },
+      {
+        title: "Cursor",
+        link: "https://cursor.so",
+        thumbnail:"/rhyssbold.jpg",
+      },
+      {
+        title: "Rogue",
+        link: "https://userogue.com",
+        thumbnail:"/rhyssbold.jpg",
+      },
+      {
+          title: "Rogue",
+          link: "https://userogue.com",
+          thumbnail:"/rhyssbold.jpg",
+      },
+      {
+        title: "Editrix AI",
+        link: "https://editrix.ai",
+        thumbnail:'/rhyssbold.jpg',
+      },
+      {
+        title: "Moonbeam",
+        link: "https://gomoonbeam.com",
+        thumbnail:"/rhyssbold.jpg",
+      },
+      {
+        title: "Cursor",
+        link: "https://cursor.so",
+        thumbnail:"/rhyssbold.jpg",
+      },
+      {
+        title: "Rogue",
+        link: "https://userogue.com",
+        thumbnail:"/rhyssbold.jpg",
+      },
+      {
+          title: "Rogue",
+          link: "https://userogue.com",
+          thumbnail:"/rhyssbold.jpg",
+      },
+      {
+        title: "Editrix AI",
+        link: "https://editrix.ai",
+        thumbnail:'/rhyssbold.jpg',
+      },
+    
+  ];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,79 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        lexend: ["Lexend", "sans-serif"]
+      },
+      colors: {
+        text: '#454545',
+        background: '#ffffff',
+        primary: '#b80206',
+        secondary: '#050315',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'bg-img-1': "url('')",
+        'bg-img-2': "url('')"
+      },
+      screens: {
+        'xs': '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
