feddbfb33017acb79d97ed3a53aab26c0c854ac5
Miguel Alino <miggyalino35@gmail.com>
2024-03-18 02:11:33 +0800

    Fixed Overflow issue

--- components/CustomerTestimonials.tsx
+++ components/CustomerTestimonials.tsx
@@ -4,7 +4,7 @@ import { TESTIMONIALS } from '@/constants'
 
 const CustomerTestimonials = () => {
   return (
-    <div className="h-[60vh] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative gap-16 my-16">
+    <div className="h-[60vh] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative gap-16 my-16 overflow-hidden">
       
       <h2 className='text-4xl font-bold'>What Our Clients Say</h2>
       
