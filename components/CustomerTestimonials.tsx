f498c70f1939935339bd03fa312e70ea1cf4c33a
Miguel Alino <miggyalino35@gmail.com>
2024-03-18 02:16:17 +0800

    fixed customer testimonials section

--- components/CustomerTestimonials.tsx
+++ components/CustomerTestimonials.tsx
@@ -4,9 +4,9 @@ import { TESTIMONIALS } from '@/constants'
 
 const CustomerTestimonials = () => {
   return (
-    <div className="h-[60vh] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative gap-16 my-16 overflow-hidden">
+    <div className="h-[80vh] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative gap-16 my-16 overflow-hidden">
       
-      <h2 className='text-4xl font-bold mt-6'>What Our Clients Say</h2>
+      <h2 className='text-4xl font-bold'>What Our Clients Say</h2>
       
       <div className=''>
         <InfiniteMovingCards
