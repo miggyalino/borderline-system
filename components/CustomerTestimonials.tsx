ea6da14f76bc0446dab4e42bc0dda2b024a0e715
Miguel Alino <miggyalino35@gmail.com>
2024-03-18 02:13:23 +0800

    fixed customer testimonials header

--- components/CustomerTestimonials.tsx
+++ components/CustomerTestimonials.tsx
@@ -6,7 +6,7 @@ const CustomerTestimonials = () => {
   return (
     <div className="h-[60vh] rounded-md flex flex-col antialiased dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative gap-16 my-16 overflow-hidden">
       
-      <h2 className='text-4xl font-bold'>What Our Clients Say</h2>
+      <h2 className='text-4xl font-bold mt-6'>What Our Clients Say</h2>
       
       <div className=''>
         <InfiniteMovingCards
