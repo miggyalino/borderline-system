91085bb3a0f4dd70fffd613f1fa14a054a6e68c8
Miguel Alino <miggyalino35@gmail.com>
2024-03-24 02:00:06 +0800

    added api routes

---
+++ app/services/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className=''>
+      <div className='padding-container max-container'>
+        <div className='h-screen py-12'>
+          <h2 className='text-black text-4xl font-bold'>Our Services</h2>
+        </div>
+        
+      </div>
+      
+    </section>
+  )
+}
+
+export default page
